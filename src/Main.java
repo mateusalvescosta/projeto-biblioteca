3179751651af6257d689db0933133a65f07e5f7c
Mateus Alves Costa <mateuscosta3497@gmail.com>
2026-03-19 11:46:59 -0300

    mudando a lógica do projeto

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,6 @@
-import br.edu.biblioteca.service.AutorService;
-import br.edu.biblioteca.service.CategoriaService;
-import br.edu.biblioteca.service.LivroService;
-
-
 public class Main {
     public static void main(String[] args) {
-        AutorService autores = new AutorService(2);
-        CategoriaService categorias = new CategoriaService(2);
-        LivroService livro = new LivroService();
 
-        autores.criarAutor("Mateus");
-        autores.criarAutor("Carlos");
-        autores.imprimirAutores();
-
-        System.out.println(" ");
-
-        categorias.criarCategoria("Romance");
-        categorias.criarCategoria("Terror");
-        categorias.imprimirCategorias();
-
-        System.out.println(" ");
-
-        livro.criarLivro("8599296361", "A Cabana", 2008, categorias.getCategorias(), autores.getAutores());
-        livro.imprimirLivro();
 
 
 
