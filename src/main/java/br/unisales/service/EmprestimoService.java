1131a5e0f605ddc8d2938a7a049260d3177596bb
Arthur Suzuki <yujisaccont@gmail.com>
2026-03-30 16:50:35 -0300

    Emprestimo(nao concluido)

---
+++ src/main/java/br/unisales/service/EmprestimoService.java
@@ -0,0 +1,47 @@
+package br.unisales.service;
+import br.unisales.database.table.Emprestimo;
+import br.unisales.database.table.Exemplar;
+
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
+public class EmprestimoService {
+    private ArrayList<Emprestimo> emprestimos;
+    public EmprestimoService() {
+        emprestimos = new ArrayList<>();
+    }
+
+    public void emprestarExemplar(long usuarioId, long exemplarId){
+        Emprestimo emp = new Emprestimo(); //
+        emp.setUsuarioId(usuarioId);
+        emp.setExemplarId(exemplarId);
+        emp.setDataDevolucaoPrevista(LocalDateTime.now().plusDays(7));
+        emp.setDataDevolucao(null);
+        emp.setDataEmprestimo(LocalDateTime.now());
+        emprestimos.add(emp); //
+    }
+
+    public void devolverExemplar(long exemplarId){
+        for(Emprestimo emp : emprestimos){ //para cada emprestimo da lista
+
+            boolean encontrado = false;
+            if(emp.getExemplarId() == exemplarId){
+                emp.setDataDevolucao(LocalDateTime.now());
+                System.out.println("Livro devolvido!");
+                encontrado = true;
+                break;
+            }
+            if (!encontrado){
+                System.out.println("Livro não encontrado!");
+            }
+        }
+    }
+
+    public void renovarExemplar (long exemplarId){
+        for(Emprestimo emp : emprestimos){
+            if(emp.getExemplarId() == exemplarId){
+                
+            }
+        }
+    }
+}
