f41ff68aad25acf7f09d90c9349e535bfcef17b3
Mateus Alves Costa <mateuscosta3497@gmail.com>
2026-04-26 01:43:02 -0300

    docs: adiciona comentarios nos enums

--- src/main/java/br/unisales/Enumeration/UsuarioTipoEnum.java
+++ src/main/java/br/unisales/Enumeration/UsuarioTipoEnum.java
@@ -1,7 +1,14 @@
 package br.unisales.Enumeration;
 
+// Enum que representa os tipos de usuário do sistema
 public enum UsuarioTipoEnum {
+
+    // Usuário do tipo aluno
     ALUNO,
+
+    // Usuário do tipo professor
     PROFESSOR,
+
+    // Usuário do tipo servidor da instituição
     SERVIDOR;
 }
